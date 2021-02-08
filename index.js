1587af3dedb6ae014bc60f075e03162d793bc4c1
GhastlyWall <leoleuiz@gmail.com>
2021-02-08 11:05:31 -0300

    commit

--- index.js
+++ index.js
@@ -5,21 +5,9 @@ function addLine(dataUser){
 
   var tr = document.createElement("tr");
 
-  tr.innerHTML = '  <tr>
-  <td>
-    <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
-  </td>
-  <td>Fulano</td>
-  <td>fulano@hcode.com.br</td>
-  <td>Sim</td>
-  <td>02/04/2018</td>
-  <td>
-    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-  </td>
-</tr>'
+  tr.innerHTML = "";
 
-
+}
 document.getElementById("form-user-create").addEventListener("submit", function(event){
 
   event.preventDefault();
